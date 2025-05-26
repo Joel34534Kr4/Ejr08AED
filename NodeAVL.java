98fbdc61665918e52514af60dd9bf18bff7f8ecb
Joel34534Kr4 <joel.ventura@estudiante.ucsm.edu.pe>
2025-05-25 19:58:28 -0500

    Update NodeAVL.java

--- NodeAVL.java
+++ NodeAVL.java
@@ -1,15 +1,16 @@
 package avltree;
-
+//modo extendido para el avl, incluyendo el factor de balance
 public class NodeAVL<E>  extends Node<E>{
     protected int bf; // balance factor
-
+    // Constructor que llama al constructor del nodo base y establece bf = 0
     public NodeAVL(E data) {
         super(data);
         bf = 0;
     }
-
+    // Sobrescribe el método toString para mostrar dato y factor de balance
     @Override
     public String toString() {
         return data + " (" + bf + ")";
     }
 }
+
